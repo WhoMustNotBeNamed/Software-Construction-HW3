11ecb3c5b8d7e6f8a60b057023d95f96e047cad6
Alex <yevsyukovaleks@gmail.com>
2024-05-24 19:15:14 +0300

    Initial commit

---
+++ ticket.sales.service/src/main/kotlin/ru/hse/ticket/sales/service/TicketSalesServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.hse.ticket.sales.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketSalesServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TicketSalesServiceApplication>(*args)
+}
---
+++ ticket.sales.service/src/test/kotlin/ru/hse/ticket/sales/service/TicketSalesServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.hse.ticket.sales.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicketSalesServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
