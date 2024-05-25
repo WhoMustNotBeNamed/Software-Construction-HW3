b8b41e73ece90b37da4dd2a8bc63a5d3889c5209
Alex <yevsyukovaleks@gmail.com>
2024-05-26 02:16:48 +0300

    update sql

--- ticket.sales.service/src/main/resources/db/changelog/01.000.00/order.sql
+++ ticket.sales.service/src/main/resources/db/changelog/01.000.00/order.sql
@@ -2,12 +2,13 @@
 -- changeset aevsyukov_1@edu.hse.ru:1 logicalFilePath:01.000.00/order.sql
 create table order
 (
-    id              uuid      default gen_random_uuid() primary key,
+    id              uuid      default gen_random_uuid()
+        primary key,
     user_id         uuid not null,
     from_station_id uuid not null,
     to_station_id   uuid not null,
     status          int  not null,
-    created         timestamp default current_timestamp(),
+    created         timestamp,
     foreign key (user_id) references user (id),
     foreign key (from_station_id) references station (id),
     foreign key (to_station_id) references station (id)
--- ticket.sales.service/src/main/resources/db/changelog/01.000.00/station.sql
+++ ticket.sales.service/src/main/resources/db/changelog/01.000.00/station.sql
@@ -2,7 +2,8 @@
 -- changeset aevsyukov_1@edu.hse.ru:1 logicalFilePath:01.000.00/station.sql
 create table station
 (
-    id      uuid default gen_random_uuid() primary key,
+    id      uuid default gen_random_uuid()
+        primary key,
     station varchar(50) not null
 )
 -- rollback delete station;
