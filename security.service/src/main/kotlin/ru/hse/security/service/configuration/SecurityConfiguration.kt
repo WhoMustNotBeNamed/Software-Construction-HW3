e7298b191a83d01a633a9f1f61352691d0986c72
Alex <yevsyukovaleks@gmail.com>
2024-06-04 16:51:06 +0300

    fix code style

--- security.service/src/main/kotlin/ru/hse/security/service/configuration/SecurityConfiguration.kt
+++ security.service/src/main/kotlin/ru/hse/security/service/configuration/SecurityConfiguration.kt
@@ -2,8 +2,6 @@ package ru.hse.security.service.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.AuthenticationProvider
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
